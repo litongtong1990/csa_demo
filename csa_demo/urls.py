3d0c6fb015505f1391974ae02a9c454d0cf9533e
ohnojack <liyichengtt@gmail.com>
2016-06-01 23:30:38 +0800

    add some new page and apis

--- csa_demo/urls.py
+++ csa_demo/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.stream, name='stream'),   
-    url(r'^stream/$', views.stream, name='stream'),  
-    url(r'^api/$', views.api, name='api'),        
+    url(r'^$', views.stream_apfloor, name='stream_apfloor'),      
+    url(r'^stream/$', views.stream, name='stream'),
+    url(r'^stream_ds/$', views.stream_ds, name='stream_ds'),        
+
+    url(r'^stream_apfloor/$', views.stream_apfloor, name='stream_apfloor'),   
+    url(r'^api_apfloor/$', views.api_apfloor, name='api_apfloor'),  
+    url(r'^api_apfloor_min/$', views.api_apfloor_min, name='api_apfloor_min'),            
 ]
--- csa_demo/views.py
+++ csa_demo/views.py
@@ -27,7 +27,18 @@ def stream(request):
 
 
 
-def api(request):
+def stream_apfloor(request):
+    user = 'xx'
+    content = {
+        'user': user
+    }
+       
+    return render(request, 'stream_apfloor.html', content)
+
+
+
+
+def api_apfloor(request):
 
     url = 'https://10.75.6.130:9443/csa/query/table/apfloor'
     method = 'get'
@@ -38,4 +49,33 @@ def api(request):
         'api_result': json.loads(api_result)
     }
 
-    return render(request, 'api.html', content)
+    return render(request, 'api_apfloor.html', content)
+
+
+def api_apfloor_min(request):
+
+    url = 'https://10.75.6.130:9443/csa/query/table/apfloor_min'
+    method = 'get'
+    token = 'eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.m_DcR_YaUv7LruAGrL1YaPoIlfKEStMNNjulHBboteTS4eiYrzPqvA.0Md5GeZdckS1iO3akYCLHw.vCK8PJ8I6A40eyILwgqtZYxCDhNvRE09MZ73zQ0gST9QKGJF9Tt7cLDkdGbybmKPmLZk6JI2DXys2CwYw5EWNTRDbVchdS2s_1hJ61VRADbaH0iVIzRhr1d1XHk0mAeMKjWIfUz2EVKqG-sdiQawlrgSvjhMkOBX_srcVTH9rW090C-4Oco2NW3e2HAfIckk_s9GrO5GDaSbKxt0nxlmF8wpTw8CRmrueYOmVqzU6VQgsgyrj8YBAUoT1mUSCqXo1BNfpEzhRzHCFkojLdk_lMVNUapiTKYmTIbS-1wZ_lH3nW-MjNHKF-RrxmihATWfgTJLLiwCMiggJJWu9hqqQACu275omJfiDqz3fBAvdydx7rYtKTXw9Hgag-673Dm5tJeVBjqUBq1MfYRBvRrkG3ZjJEx7Fpr_IbGtAfOlDBO70s3lmF9M4ZuSvcLh39wRzmIShaoSiJwZqx2-v4wYlmSSOd3KW-eq3R0QR1slYreeIr23TQKPnxU81T001YGq2bQOgfnCsUZnJ1BECt3u0Xrh5pI6ekhl_8b6g461lSc8EemaFbrnryERZrJ-71ZTTl0pSCggI8d8pUQLZmMSjILbMYBR89Q8mbjh-S6pQWFQaF_js0sSU4uOWfWdh7sHCDJwL_REaRv5sE960ff6ZAKgcwdArL75RTCtP7ca-cYGfM_W0VZw_HBjYGsbfatqvZK0FAK6UmFq9qQjqBwpVPqz3vOo-sq7Bv7KTIl853lD200G2AmysV3u91-h32zlJ15l3cDqqC9GFZlVdCJJYBGnx1yx-9dSklRCYinLZQwlQ-5thC-Q4y4kG3yR_t_hnrbtOdrtBc1jBjL2RdpPff-WzClfVMWanoYIuH5E707vHJg_wD28Zm7ErYTmp1kTofeCHODtunJJ2OqbAjkvVfBPmcDYDNYS4ROBU0scx4xBzjYzcLyso8sneuqOebn-cPExwSD45bIZLZIitX_Dn-1WktoeJcZ6Oxzv4KzKxD2x1Ljj0oPKX-nCq5u71Ier92VdfDUZZmvD0uWU7pVZmktyHGjjXa-_I8l_oDQj_Fvu1d6f592I2B7GAZvScmNTN2lA5YPmSuPIlmHHoSbVTxYDlohVlvn944SuMHI9pwIMFcqSHiOmTjJXOPl4_kBXbYWVtgcrzNak0Vz1UoG7BJgqGHpV0Qcp9gKkfwASy3aG1wLqFYRdTRZ4eZZhtPRGHMu0jS7OBzqaGl7mu7_o2kWPYc3NZyngyprTx-xx2gMi78YmoXE4NXniZtFftwxv.R7Rf6XxvLTsvB0Jd1et84g'
+    payload = ''
+    api_result = API_Request(url,method,token,payload)
+    content = {
+        'api_result': json.loads(api_result)
+    }
+
+    return render(request, 'api_apfloor.html', content)
+
+
+
+
+
+def stream_ds(request):
+    user = 'xx'
+    content = {
+        'user': user
+    }
+       
+    return render(request, 'demo_ds.html', content)
+
+
+
